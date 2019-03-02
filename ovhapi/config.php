919df5360c06bfe8d9760bf8fb34e10ba415e101
Germain Carré <germaincarre@yahoo.fr>
2019-03-02 22:39:48 +0100

    update

--- ovhapi/config.php
+++ ovhapi/config.php
@@ -5,7 +5,7 @@ require __DIR__ . '/vendor/autoload.php';
 $endpoint = 'ovh-eu';
 $applicationKey = "7pafXqPQ7jiA2Y4u";
 $applicationSecret = "HD4S4MHrtkMZMKMRYbzgu7SrI1KK8Gb5";
-$redirection = "https://vscope.carsso.ovh";
+$redirection = "https://vscope.carsso.dev";
 $rights = array( (object) [
     'method'    => 'GET',
     'path'      => '/*'
